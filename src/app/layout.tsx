8dd9d95b4290e30a0e6d42ecc44a54bf090b35bd
Muhammadsyifasurya <syifamuhammad3139@gmail.com>
2025-12-11 11:23:46 +0700

    delete : remove cms content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
-import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -69,6 +68,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
       >
+        {" "}
+        <Header />
         {children}
       </body>
     </html>
